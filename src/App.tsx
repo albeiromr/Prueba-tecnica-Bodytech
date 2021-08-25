264a3cdde710c2f1a8d0d94120c215ddb9f62ea7
Albeiro Marin <xlandru@hotmail.com>
2021-08-25 17:14:29 -0500

    Create the app main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import MainContainer from './containers/main container/MainContainer';
 
 function App() {
   return (
     <div className="App">
+      <MainContainer />
     </div>
   );
 }
---
+++ src/containers/main container/MainContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./0px-599px.scss";
+import "./600px-1024px.scss";
+import "./1025px-1920px.scss";
+
+const MainContainer = () => {
+
+    return (
+        <div className="main-container"></div>
+    )
+}
+
+export default MainContainer
