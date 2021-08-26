180a42e20cb4940cb80412ab75c9ed981f4c118f
Albeiro Marin <xlandru@hotmail.com>
2021-08-26 11:42:58 -0500

    Reads Search input value when user types

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import "./0px-599px.scss";
 import "./600px-1024px.scss";
 import "./1025px-1920px.scss";
@@ -6,15 +6,32 @@ import logoDesktop from "../../assets/logo/logoDesktop.png";
 
 const Header = () => {
 
+    const searchForm = useRef<HTMLFormElement>(null)
+
+    const [songName, setSongName] = useState<string>("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>):void => {
+        setSongName(e.target.value);
+    }
+
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>):void => {
+        e.preventDefault();
+        //
+        searchForm.current?.reset();
+        setSongName("");
+
+    }
+
     return (
         <header className="header">
             <div className="header__green-bar"></div>
             <img className="header__logo" src={logoDesktop} alt="company logo" />
-            <form className="header__form">
+            <form ref={searchForm} onSubmit={handleSubmit} className="header__form">
                 
                 <label className="header__lable" htmlFor="song">Find your song</label>
 
                 <input 
+                onChange={handleChange}
                 className="header__input"
                 id="song"
                 type="text" 
